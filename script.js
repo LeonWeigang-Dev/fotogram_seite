d328c42c6b056f82f0a67ab4898392374cd0f12b
Leon W. <orlando.game@web.de>
2026-02-18 17:06:34 +0100

    feat: add image dialog functionality and enhance album section

---
+++ script.js
@@ -0,0 +1,37 @@
+let myImgs = [
+    "alaska.jpg",
+    "anime.jpg",
+    "atmosphere.jpg",
+    "blue_tit.jpg",
+    "hurricane.jpg",
+    "lake.jpg",
+    "moorente.jpg",
+    "sea.jpg",
+    "snow_bunting.jpg",
+    "snow_leopard.jpg",
+    "travel.jpg",
+    "winter.jpg"
+];
+let myNames = [
+    "Alaska",
+    "Anime Stadt",
+    "Atmosphärische Wolken",
+    "Blaumeise",
+    "Hurrikan von Erdorbit",
+    "See",
+    "Moorente Foto",
+    "Mann bei Meer",
+    "Schneeammer Vogel",
+    "Schnee-Leoparden",
+    "Berglandschaft",
+    "Schneebedeckter Baum"
+]
+
+const dialogRef = document.getElementById("myDialog");
+
+function openDialog() {
+    dialogRef.showModal();
+}
+function closeDialog() {
+    dialogRef.close();
+}
